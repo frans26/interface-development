e45071d389800ecd7fbe5cc5fe7a58b340b046de
Francis M <francis.mac626@gmail.com>
2023-08-24 21:20:22 +0800

    add constants

---
+++ src/constants/blog-list.ts
@@ -0,0 +1,25 @@
+import { Blog } from "../models/blog.model";
+
+export const BLOG_LIST: Blog[] = [
+  {
+    id: 1,
+    img: "seafoods",
+    title: "Summer Lunch Menu By Mark Best",
+    description:
+      "AEG ambassador Mark Best's summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home",
+  },
+  {
+    id: 2,
+    img: "chicken",
+    title: "Summer Lunch Menu By Mark Best",
+    description:
+      "One of Australia's best chefs and AEG ambassador Mark Best, shares his favourite Christmas Eve menu which is sure to impress your guest",
+  },
+  {
+    id: 3,
+    img: "chicken",
+    title: "Taking Taste Further",
+    description:
+      "This exdusive cookbook gives you al the know-how you need. We've designed it to make sure you get the most out of our products and the best out of your food.",
+  },
+];
---
+++ src/constants/image-list.ts
@@ -0,0 +1,16 @@
+import Person from "../assets/component-01/Image-01@2x.jpg";
+import Fruits from "../assets/component-01/Image-02@2x.jpg";
+import Veggies from "../assets/component-01/Image-03@2x.jpg";
+
+import Seafoods from "../assets/component-02/Image-01@2x.jpg";
+import Chicken from "../assets/component-02/Image-02@2x.jpg";
+import Stew from "../assets/component-02/Image-03@2x.jpg";
+
+export const IMAGES = {
+  person: Person,
+  fruits: Fruits,
+  veggies: Veggies,
+  seafoods: Seafoods,
+  chicken: Chicken,
+  stew: Stew,
+};
